57e33dfb236344642ef0c79c0a240a7a8aacf422
“Muzyria” <fila090580@gmail.com>
2023-07-19 14:08:11 +0300

    new

--- my_page/week_days/urls.py
+++ my_page/week_days/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('monday', views.monday),
-    path('tuesday', views.tuesday),
+    path('monday/', views.monday),
+    path('tuesday/', views.tuesday),
 ]
