9f0e11fb5157377e23d0f759715793f0a4aa8970
“Muzyria” <fila090580@gmail.com>
2023-07-19 16:59:16 +0300

    new

---
+++ my_site/blog/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+
+def main_page(request):
+    return HttpResponse("Главная страница")
+
+
+def posts(request):
+    return HttpResponse("Все посты блога")
+
