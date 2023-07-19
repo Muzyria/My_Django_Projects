994093700a429f454e5458fb22f8250273e86f56
“Muzyria” <fila090580@gmail.com>
2023-07-19 14:12:38 +0300

    new

--- my_page/my_page/urls.py
+++ my_page/my_page/urls.py
@@ -20,6 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('horoscope/', include('horoscope.urls')),
-
     path('todo_week/', include('week_days.urls')),
 ]
