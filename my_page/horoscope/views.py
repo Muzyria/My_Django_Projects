df73af51ff1686d15cadf5c22723a111d8b1e0fa
“Muzyria” <fila090580@gmail.com>
2023-07-19 17:31:38 +0300

    new

--- my_page/horoscope/views.py
+++ my_page/horoscope/views.py
@@ -1,53 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 
 # Create your views here.
 
 
-def aries(request):
-    return HttpResponse("Знак зодиака Овен")
+def get_info_about_sign_zodiac(request, sign_zodiac):
+    signs = {
+        'aries': ["♈", "Овен - первый знак зодиака, планета Марс (с 21 марта по 20 апреля)."],
+        'taurus': ["♉", "Телец - второй знак зодиака, планета Венера (с 21 апреля по 21 мая)."],
+        'gemini': ["♊", "Близнецы - третий знак зодиака, планета Меркурий (с 22 мая по 21 июня)."],
+        'cancer': ["♋", "Рак - четвёртый знак зодиака, Луна (с 22 июня по 22 июля)."],
+        'leo': ["♌", "Лев - пятый знак зодиака, солнце (с 23 июля по 21 августа)."],
+        'virgo': ["♍", "Дева - шестой знак зодиака, планета Меркурий (с 22 августа по 23 сентября)."],
+        'libra': ["♎", "Весы - седьмой знак зодиака, планета Венера (с 24 сентября по 23 октября)."],
+        'scorpio': ["♏", "Скорпион - восьмой знак зодиака, планета Марс (с 24 октября по 22 ноября)."],
+        'sagittarius': ["♐", "Стрелец - девятый знак зодиака, планета Юпитер (с 23 ноября по 22 декабря)."],
+        'capricorn': ["♑", "Козерог - десятый знак зодиака, планета Сатурн (с 23 декабря по 20 января)."],
+        'aquarius': ["♒", "Водолей - одиннадцатый знак зодиака, планеты Уран и Сатурн (с 21 января по 19 февраля)."],
+        'pisces': ["♓", "Рыбы - двенадцатый знак зодиака, планеты Юпитер (с 20 февраля по 20 марта)."]
+    }
 
-
-def taurus(request):
-    return HttpResponse("Знак зодиака Телец")
-
-
-def gemini(request):
-    return HttpResponse("Знак зодиака Близнецы")
-
-
-def cancer(request):
-    return HttpResponse("Знак зодиака Рак")
-
-
-def leo(request):
-    return HttpResponse("Знак зодиака Лев")
-
-
-def virgo(request):
-    return HttpResponse("Знак зодиака Дева")
-
-
-def libra(request):
-    return HttpResponse("Знак зодиака Весы")
-
-
-def scorpio(request):
-    return HttpResponse("Знак зодиака Скорпион")
-
-
-def sagittarius(request):
-    return HttpResponse("Знак зодиака Стрелец")
-
-
-def capricorn(request):
-    return HttpResponse("Знак зодиака Козерог")
-
-
-def aquarius(request):
-    return HttpResponse("Знак зодиака Водолей")
-
-
-def pisces(request):
-    return HttpResponse("Знак зодиака Рыбы")
+    if sign_zodiac.lower() in signs:
+        return HttpResponse(signs[sign_zodiac.lower()])
+    return HttpResponseNotFound(f"Неизвестный знак зодиака {sign_zodiac}")
