e694484013c7e192dcbe4243e5a80e803cd44059
“Muzyria” <fila090580@gmail.com>
2023-07-27 16:41:12 +0300

    new

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.main_page),
     path('posts/', views.posts),
+
+    path('posts/people', views.people_list),
+
     path('posts/<int:number_post>/', views.get_info_about_name_post_by_number),
     path('posts/<str:name_post>/', views.get_info_about_name_post),
 
